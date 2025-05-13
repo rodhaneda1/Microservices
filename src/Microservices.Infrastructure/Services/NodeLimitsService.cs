db583097c77a022f878a0e74963334270c94603b
Rodrigo Haneda <104365362+rodhaneda1@users.noreply.github.com>
2025-05-13 14:59:23 -0300

    Update NodeLimitsService.cs

--- src/Microservices.Infrastructure/Services/NodeLimitsService.cs
+++ src/Microservices.Infrastructure/Services/NodeLimitsService.cs
@@ -29,33 +29,5 @@ namespace Microservices.Infrastructure.Services
             var timeOut = string.IsNullOrEmpty(_settings.Timeout) ? 30000 : Convert.ToInt32(_settings.Timeout);
             _client.Timeout =  TimeSpan.FromSeconds(timeOut);
         }
-
-        public async Task<IEnumerable<GetFavoriteLimitResponseDto>> GetFavoriteLimitsAsync(GetFavoriteLimitRequestDto request)
-        {
-            var url = _settings.UrlService + $"/favored/limits/{request.agencyNumberOrig}/{request.accountNumberOrig}/{request.documentOrig}/{request.documentFav}/{request.product}";
-            _log.LogInformation($"GetFavoriteLimits - {url} - {JsonSerializer.Serialize(request)}");
-            var response = await _client.GetAsync(url);
-
-            return await DeserializarObjetoResponse<IEnumerable<GetFavoriteLimitResponseDto>>(response);
-        }
-
-        public async Task<IEnumerable<GetLimitResponseDto>> GetLimitsAsync(GetLimitRequestDto request)
-        {
-            var url = _settings.UrlService + $"/limits/{request.cpfCnpj}/{request.agency}/{request.accountNumber}";
-            _log.LogInformation($"GetLimits - {url} - {JsonSerializer.Serialize(request)}");
-            var response = await _client.GetAsync(url);
-
-            return await DeserializarObjetoResponse<IEnumerable<GetLimitResponseDto>>(response);
-        }
-
-        public async Task<PostLimitsResponseDto> PostLimitsAsync(PostLimitsRequestDto request)
-        {
-            var url = _settings.UrlService + $"/limits/{request.cpfCnpj}/{request.agency}/{request.accountNumber}/{request.product}";
-            var nodeRequest = _mapper.Map<PostLimitDto>(request);
-            _log.LogInformation($"Post Limits - {url} - {JsonSerializer.Serialize(request)}");
-            var response = await _client.PostAsync(url, ObterConteudo(nodeRequest));
-
-            return await DeserializarObjetoResponse<PostLimitsResponseDto>(response);
-        }
     }
 }
